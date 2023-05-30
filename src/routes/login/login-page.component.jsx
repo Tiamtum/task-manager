13a67fe7a306597072390ea1ca31ea2079d842dd
Morgan Chafe <mc.github@outlook.com>
2023-05-30 14:12:45 -0230

    Update login-page.component.jsx

--- src/routes/login/login-page.component.jsx
+++ src/routes/login/login-page.component.jsx
@@ -1,8 +1,12 @@
+import { Link } from "react-router-dom";
+import FormComponent from "../../components/Navbar/Form.component";
+
 export default function LoginPage(){
     return(
-        <div>
-            <p>login page.</p>
-            <a href="/">Go back</a>
-        </div>
+        <>
+        <FormComponent name={"Login"} buttonText={"Login"} action={"#"}/>
+        <p className="text-center">Don't have an account? Click <Link to="/register">here</Link> to register.</p>
+        </>
     );
+    
 }
