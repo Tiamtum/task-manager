51034b8f60c3be879d04bcf4f4440dae1b19c945
Morgan Chafe <mc.github@outlook.com>
2023-05-30 14:12:52 -0230

    Update root.component.jsx

--- src/routes/root.component.jsx
+++ src/routes/root.component.jsx
@@ -1,11 +1,19 @@
-import NavBar from "../components/NarBar/navbar.component"
+import {Link} from "react-router-dom";
+import ColoredLine from "../utils/components/ColoredLine.component";
+
+
 
 export default function Root() {
     return(        
-        <main>
-        <NavBar text="Task Manager" buttonText="Contact" />
-        <h1>Landing Page</h1>
-        <a href="/login">login</a>
-        </main>       
+        <div className="jumbotron text-center my-5">
+        <h1 className="display-4">Welcome to Task Manager</h1>
+        <p className="lead">List making application inspired by Trello. </p>
+        <ColoredLine color={"Black"} className="my-4"/>
+        <p>Login to start making lists</p>
+        <p className="lead">
+            <Link to="/login" className="btn btn-primary btn-lg" role="button">Login</Link>
+        </p>
+        <Link to="/dashboard">Dashboard</Link>
+        </div>   
     );
   }
