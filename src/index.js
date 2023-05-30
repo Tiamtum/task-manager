67bbecb26423135ffb87caad7924646a2f8a4dde
Morgan Chafe <mc.github@outlook.com>
2023-05-30 14:12:23 -0230

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,28 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import App from "./App"
 import 'bootstrap/dist/css/bootstrap.css';
 import "./App.css";
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import Root from './routes/root.component';
-import ErrorPage from './routes/error-page.component';
-import LoginPage from "./routes/login/login-page.component"
+import { BrowserRouter } from 'react-router-dom';
+// import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+// import Root from './routes/root.component';
+// import ErrorPage from './routes/error-page.component';
+// import LoginPage from "./routes/login/login-page.component"
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Root />,
-    errorElement: <ErrorPage />
-  },
-  {
-    path: "/login",
-    element: <LoginPage />
-  }
-])
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <Root />,
+//     errorElement: <ErrorPage />
+//   },
+//   {
+//     path: "/login",
+//     element: <LoginPage />
+//   }
+// ])
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
