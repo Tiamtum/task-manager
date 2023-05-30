15203f2afcf2a5249e7968bc3b8755dc33efe7c0
Morgan Chafe <mc.github@outlook.com>
2023-05-30 14:12:48 -0230

    Create register-page.component.jsx

---
+++ src/routes/register/register-page.component.jsx
@@ -0,0 +1,8 @@
+import FormComponent from "../../components/Navbar/Form.component";
+
+export default function RegisterPage(){
+    return(
+        <FormComponent name={"Register"} buttonText={"Register"} action={"#"}/>
+    );
+    
+}
