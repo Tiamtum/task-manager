7bba228f20b7b2269d6d658013cf9ade632c48bd
Morgan Chafe <mc.github@outlook.com>
2023-06-01 14:26:23 -0230

    Update ListItem.component.jsx

--- src/components/List/ListItem/ListItem.component.jsx
+++ src/components/List/ListItem/ListItem.component.jsx
@@ -1,15 +1,46 @@
+import { Link } from "react-router-dom";
+import  Button  from "react-bootstrap/Button";
+import  Modal  from "react-bootstrap/Modal";
+
+import { useState } from "react";
+import DetailedListItem from "./DetailedListItem/DetailedListItem.component";
 export default function ListItem({task})
 {
-    console.log("list item render")
+    //taskName:"",summary:"",description:"",priority:"",due:"",tags:""
+
+    const {taskName, summary, description, priority, due, tags,id} = task;
+    console.log("list item render", task)
+
+    const [show, setShow] = useState(false);
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
+
     return(
-        // <div>
-        //     {task.taskName + ", "}
-        //     {task.shortDesc + ", "}
-        //     {task.longDesc + ", "}
-        //     {task.priority + ", "}
-        //     {task.due + ", "}
-        //     {task.tags + " " }
-        // </div>
-        <div>List item!</div>
+        <>
+           <span id="task" className="col">{taskName}</span>            
+           <span id="prio" className="col">{priority}</span>
+           <span id="due" className="col">{due}</span> 
+           <span id="summary" className="col">{summary}</span>
+            {/* <span id="details" className="col"><Link to={`/list/${id}`}>Details</Link></span> */}
+            
+            <span className="col"><Button variant="outline-primary" onClick={handleShow}>Details </Button></span>
+            <Modal show={show} onHide={handleClose}>
+            <Modal.Header closeButton>
+            <Modal.Title>{taskName}</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                <h4>{summary}</h4>
+                <p>{description}</p>
+                <p>{due}</p>
+                <p>{priority}</p>
+            </Modal.Body>
+            <Modal.Footer>
+            <Button variant="secondary" onClick={handleClose}>
+                Close
+            </Button>
+            </Modal.Footer>
+            </Modal>
+        </>
     );
 }
