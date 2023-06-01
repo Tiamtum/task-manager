22f95895d20586d24d477db282cefb3f8103e912
Morgan Chafe <mc.github@outlook.com>
2023-06-01 14:25:59 -0230

    Changed routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import { Route, Routes } from "react-router-dom";
 import MyNavbar from "./components/Navbar/Navbar.component";
+import LandingPage from "./components/LandingPage/LandingPage.component";
 import List from "./components/List/List.component";
-import ListItem from "./components/List/ListItem/ListItem.component"
 import AddItem from "./components/List/AddItem/AddItem.component"
 import AboutPage from "./components/AboutPage/AboutPage.component";
 import ErrorPage from "./components/ErrorPage/ErrorPage.component";
+import DetailedListItem from "./components/List/ListItem/DetailedListItem/DetailedListItem.component";
 
 //<Route path="/parent" element={<parentLayout />}
 //  Route index element={<firstRoute /}
@@ -12,17 +13,17 @@ import ErrorPage from "./components/ErrorPage/ErrorPage.component";
 //  Route path=".." element={<thirdRoute /}
 
 export default function App() {
-  
   return (
     <>
     <MyNavbar brandText={"Task Manager"} buttonText={"About"} />
     <Routes>
-        <Route path="/" element={<List />}/>
-          <Route path="/list/add" element={<AddItem />}/>
-          <Route path="/list/view" element={<ListItem />}/>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/list" element={ <List />} />
+          <Route path="/list/add" element={ <AddItem />} />
+          <Route path="/list/:id" element={ <DetailedListItem />} />
         <Route />
-        <Route path="/about" element={<AboutPage />}/>
-        <Route path="*" element={<ErrorPage />}/>
+        <Route path="/about" element={ <AboutPage />} />
+        <Route path="*" element={ <ErrorPage /> } />
     </Routes>
     </>
   );
