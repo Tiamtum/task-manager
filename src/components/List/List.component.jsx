1ee7ecb6bcc2e476fe80a50d0983c92b7e4b50f0
Morgan Chafe <mc.github@outlook.com>
2023-06-05 14:42:22 -0230

    Update List.component.jsx
    Updated to fetch tasks from a json-server database.

--- src/components/List/List.component.jsx
+++ src/components/List/List.component.jsx
@@ -1,6 +1,7 @@
-import { useState } from "react";
-import { Link, Outlet } from "react-router-dom";
+import { useEffect, useState } from "react";
+import {Link} from "react-router-dom";
 import ListItem from "./ListItem/ListItem.component";
+import ColoredLine from "./ColoredLine/ColoredLined.component";
 
 
 import "./List.styles.css";
@@ -8,12 +9,32 @@ import "./List.styles.css";
 export default function List()
 {
     //https://react.dev/learn/updating-arrays-in-state
-    const [tasks, setTasks] = useState([{taskName:"task",summary:"summary",description:"descrip",priority:"prio",due:"6/1/2023",tags:"React", id:0}]);
+    const [tasks, setTasks] = useState([]);
+
+    useEffect(()=>{
+      fetch("http://localhost:3030/tasks",
+      {
+        method: "GET"
+      })
+      .then(response => response.json())
+      .then(result => {
+        console.log("fetched: ",result);  
+        setTasks([...result]);
+      })
+      .catch(error => console.log("error",error));
+    },[])
+  
 
     console.log("list render", tasks)
     return(
         <div id="List" className="container-lg mt-5">
             <div className="row justify-content-around">
+              <span className="col mb-2"><strong>Task</strong></span>            
+              <span className="col mb-2"><strong>Priority</strong></span>
+              <span className="col mb-2"><strong>Due</strong></span> 
+              <span className="col mb-2"><strong>Summary</strong></span>           
+              <span className="col mb-2"><strong>More Details</strong></span>
+              <ColoredLine color="black" />
                     {
                         tasks.map(task=>{
                             return(
@@ -22,7 +43,8 @@ export default function List()
                         })
                     }
             </div>
-            <Link to="/list/add" className="btn btn-outline-dark float-end mt-3" state={tasks} >Add an item</Link>
+
+            <Link to="/list/add" className="btn btn-outline-dark float-end mt-5" state={tasks} >Add an item</Link>
         </div>
     );
 
