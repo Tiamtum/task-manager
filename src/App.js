7313d95cfa838b9fca67f02082380c3f56408914
Morgan Chafe <mc.github@outlook.com>
2023-06-05 14:41:35 -0230

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+
 import { Route, Routes } from "react-router-dom";
 import MyNavbar from "./components/Navbar/Navbar.component";
 import LandingPage from "./components/LandingPage/LandingPage.component";
@@ -5,14 +6,11 @@ import List from "./components/List/List.component";
 import AddItem from "./components/List/AddItem/AddItem.component"
 import AboutPage from "./components/AboutPage/AboutPage.component";
 import ErrorPage from "./components/ErrorPage/ErrorPage.component";
-import DetailedListItem from "./components/List/ListItem/DetailedListItem/DetailedListItem.component";
 
-//<Route path="/parent" element={<parentLayout />}
-//  Route index element={<firstRoute /}
-//  Route path=".." element={<secondRoute /}
-//  Route path=".." element={<thirdRoute /}
+export default function App() {  
+  
+  console.log("App render");
 
-export default function App() {
   return (
     <>
     <MyNavbar brandText={"Task Manager"} buttonText={"About"} />
@@ -20,7 +18,6 @@ export default function App() {
         <Route path="/" element={<LandingPage />} />
         <Route path="/list" element={ <List />} />
           <Route path="/list/add" element={ <AddItem />} />
-          <Route path="/list/:id" element={ <DetailedListItem />} />
         <Route />
         <Route path="/about" element={ <AboutPage />} />
         <Route path="*" element={ <ErrorPage /> } />
