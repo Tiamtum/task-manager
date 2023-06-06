e2943be31f867794ee0301fe28f866a66c6814d5
Morgan Chafe <mc.github@outlook.com>
2023-06-06 12:32:10 -0230

    Update AddItem.component.jsx

--- src/components/List/AddItem/AddItem.component.jsx
+++ src/components/List/AddItem/AddItem.component.jsx
@@ -1,11 +1,19 @@
-import MyForm from "./MyForm.component";
+import MyForm from "../../MyForm/MyForm.component";
 export default function AddItem()
 {
     console.log("additem render");
     return(
         <div id="AddItem" className="container-lg mt-5">
             <div className="column justify-content-start">
-            <MyForm />
+            <MyForm
+            method={"POST"}
+            taskName={""}
+            summary={""}
+            description={""}
+            priority={""}
+            due={""}
+            id={""}
+            buttonText={"Add"} />
             </div>
         </div>
     );
