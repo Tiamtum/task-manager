dec0cf9356bcb24b9aebd7d7cc947d982a07f7d9
Morgan Chafe <mc.github@outlook.com>
2023-06-06 12:32:25 -0230

    Update Navbar.component.jsx

--- src/components/Navbar/Navbar.component.jsx
+++ src/components/Navbar/Navbar.component.jsx
@@ -2,12 +2,12 @@ import {Link} from "react-router-dom";
 export default function Navbar({brandText,buttonText}){
 
     return(
-      <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
       <Link to="/" className="navbar-brand mx-3">{brandText}</Link>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
+      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
       </button>
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <Link to="/list" className="mx-3 link-light" >My list</Link>
       </div>
       <Link to="/about" className="btn btn-info mx-3" role="button">{buttonText}</Link>
